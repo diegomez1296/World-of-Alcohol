b6c5656ba18a27436a2ee9400e6d2b35e5a5880b
Przemek <pchab@op.pl>
2019-05-29 00:25:07 +0200

    Revert "Added entities and repositories."
    This reverts commit 524469372c18a21c9729a47ab3ab803cfb97c3f8.

--- src/main/java/dreamteam/Repositories/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package dreamteam.Repositories;
-
-import dreamteam.DAO.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends CrudRepository<Order, Long> {
-}
