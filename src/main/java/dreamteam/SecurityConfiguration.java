1c7d21138e10ecf90c93d98c20b2b2bfd97c50ce
MSI <email.pawel.minda@gmail.com>
2019-05-30 00:38:20 +0200

    WOA v0.0.6 || Added Security System - bug fix

---
+++ src/main/java/dreamteam/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package dreamteam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public SecurityConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/test1").permitAll()
+                .antMatchers("/api/test2").hasRole("ADMIN")
+                .antMatchers("/api/test3").hasAnyRole("USER","ADMIN")
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll();
+        http.headers().frameOptions().disable();
+    }
+}
