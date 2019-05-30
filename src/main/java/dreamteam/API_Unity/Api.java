5898dd49b338ab19cacdd9e0a63f10d855b98096
Lukasz Rydziński <rlukasz1996@gmail.com>
2019-05-30 19:06:14 +0200

    WOA v0.0.6 || Unity Api update

--- src/main/java/dreamteam/API_Unity/Api.java
+++ src/main/java/dreamteam/API_Unity/Api.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class Api {
@@ -12,6 +15,17 @@ public class Api {
     @GetMapping("/login/{username}/{userpasswd}")
     public String logIn(@PathVariable("username") String login, @PathVariable("userpasswd") String passwd)
     {
-        return login + " || " + passwd;
+        //Find favourite alcohol
+        StringBuilder builder = new StringBuilder();
+        List<AlcoholClassApi> alcoholClassApis = new ArrayList<>();
+        alcoholClassApis.add(new AlcoholClassApi("Rum", "Camptain Morgan is the best", "url"));
+        alcoholClassApis.add(new AlcoholClassApi("Beer", "Only true chocolate can create a real beer", "url2"));
+
+        for (AlcoholClassApi item : alcoholClassApis) {
+            builder.append(item.toString() + System.lineSeparator());
+        }
+
+
+        return builder.toString();
     }
 }
