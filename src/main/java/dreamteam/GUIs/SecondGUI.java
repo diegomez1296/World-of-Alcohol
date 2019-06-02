cf53403d2a12b4cdb1218f02a033612803dda590
Lukasz Rydziński <rlukasz1996@gmail.com>
2019-06-02 22:50:55 +0200

    WOA v0.1.4 || SecondGUI reformat (fix CI build)

--- src/main/java/dreamteam/GUIs/SecondGUI.java
+++ src/main/java/dreamteam/GUIs/SecondGUI.java
@@ -3,23 +3,10 @@ package dreamteam.GUIs;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.VaadinRequest;
-import com.vaadin.flow.server.VaadinService;
-import com.vaadin.flow.server.VaadinServletResponse;
-import com.vaadin.flow.server.VaadinSession;
-import sun.plugin.javascript.navig.Link;
-
-import javax.annotation.Resource;
-
-import static sun.plugin.javascript.navig.JSType.URL;
 
 @Route("nextView")
 public class SecondGUI extends VerticalLayout {
@@ -27,11 +14,7 @@ public class SecondGUI extends VerticalLayout {
     private Text text;
     private Icon icon;
     private TextArea textArea;
-
-
     private Button button;
-
-
 
     public SecondGUI() {
 
