0091e439087411510ff4cabfcc5d328d7b5500f9
Przemek <pchab@op.pl>
2019-06-10 14:09:31 +0200

    WOA v0.3.4 || Fixed problem with tests

--- src/test/java/dreamteam/RepositoriesTest/UserRepoTest.java
+++ src/test/java/dreamteam/RepositoriesTest/UserRepoTest.java
@@ -2,22 +2,25 @@ package dreamteam.RepositoriesTest;
 
 import dreamteam.DAO.User;
 import dreamteam.Repositories.UserRepo;
+import lombok.NoArgsConstructor;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class UserRepoTest {
 
-    private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Autowired
-    UserRepo userRepo;
+    private UserRepo userRepo;
 
     @Test
     public void createUserTest() {
