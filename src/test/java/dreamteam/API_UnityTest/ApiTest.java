57ee4f1dc402a2aafebb139eb77134ead8a4b4c6
Przemek <pchab@op.pl>
2019-06-10 15:58:58 +0200

    WOA v0.3.4 || Fixed problem with tests

--- src/test/java/dreamteam/API_UnityTest/ApiTest.java
+++ src/test/java/dreamteam/API_UnityTest/ApiTest.java
@@ -1,7 +1,11 @@
 package dreamteam.API_UnityTest;
 
+import dreamteam.DAO.Alcohol;
+import dreamteam.DAO.Favourite;
 import dreamteam.DAO.User;
 import dreamteam.General.Constans;
+import dreamteam.Repositories.AlcoholRepo;
+import dreamteam.Repositories.FavouriteRepo;
 import dreamteam.Repositories.UserRepo;
 import org.junit.Assert;
 import org.junit.Test;
@@ -13,26 +17,55 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ApiTest {
 
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private FavouriteRepo favouriteRepo;
+    @Autowired
+    private AlcoholRepo alcoholRepo;
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-
     @Test
-    public void apiTest() {
-
-        userRepo.save(new User("user", passwordEncoder.encode("user")));
-
-
+    public void apiTestLogin() {
         String uri = Constans.getAPP_URL()+"/api/login/user/user";
 
         RestTemplate restTemplate = new RestTemplate();
         Boolean result = restTemplate.getForObject(uri, Boolean.class);
         Assert.assertEquals(true,result);
     }
+
+//    @Test
+//    public void apiTestFav(){
+//        String uri = Constans.getAPP_URL()+"/api/login/admin/getFav";
+//        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+//        User user = userRepo.findUserByUsername("admin");
+//
+//
+//        List<Favourite> favourite = favouriteRepo.findAllByStringUserId(user.getUserId()+"");
+//        List<Alcohol> alcoholList = new ArrayList<>();
+//
+//        for (Favourite item : favourite) {
+//            alcoholList.add(alcoholRepo.findAlcoholById(item.getAlcohol_id()));
+//        }
+//
+//        StringBuilder builder = new StringBuilder();
+//        for (Alcohol item : alcoholList) {
+//            builder.append(item.toString());
+//            builder.append("|");
+//        }
+//
+//        String resultFav = builder.toString();
+//
+//        Assert.assertEquals(resultFav, result);
+//
+//    }
 }
