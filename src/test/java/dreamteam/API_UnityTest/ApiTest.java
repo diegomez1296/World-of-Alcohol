1a3083db3b12fb9efe064558b1bd576b62db60d3
Przemek <pchab@op.pl>
2019-06-10 15:13:37 +0200

    WOA v0.3.4 || Fixed problem with tests

--- src/test/java/dreamteam/API_UnityTest/ApiTest.java
+++ src/test/java/dreamteam/API_UnityTest/ApiTest.java
@@ -5,24 +5,33 @@ import dreamteam.Repositories.UserRepo;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class ApiTest {
 
+    @Autowired
     private UserRepo userRepo;
-    private PasswordEncoder passwordEncoder;
-    private String username;
-    private String passwd;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
 
     @Test
-    public void logInTest(String username,String passwd){
-        User user = userRepo.findUserByUsername(username);
+    public void apiTest() {
+
+        userRepo.save(new User("user", passwordEncoder.encode("user")));
 
 
-        Assert.assertTrue(passwordEncoder.matches(passwd, user.getPassword()));
+        String uri = "http://localhost:8080/api/login/user/user";
+
+        RestTemplate restTemplate = new RestTemplate();
+        Boolean result = restTemplate.getForObject(uri, Boolean.class);
+        Assert.assertEquals(true,result);
     }
 }
